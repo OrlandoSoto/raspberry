75efe5422e8a35fcb7b8cbaa98726d47964b60f2
OrlandoSoto <Orlando.Soto-Pastor@cfpb.gov>
2018-09-09 10:22:30 -0400

    Add random file selection

--- play_sounds.py
+++ play_sounds.py
@@ -1,7 +1,29 @@
+#! /usr/bin/env python
+from os import listdir
+from os.path import isfile, join 
+import random
 import pygame
+
+file_path = 'warn/'
+
+def get_file_name():
+    onlyfiles = [f for f in listdir(file_path) if isfile(join(file_path, f))]
+    print(onlyfiles)
+
+    random_filename = random.choice(onlyfiles)
+    print(random_filename)
+    return random_filename
+
+if not listdir(file_path) :
+    raise FileNotFoundError("Directory is empty")
+
+filename = get_file_name()
+
 pygame.mixer.init()
-pygame.mixer.music.load("funny.mp3")
+pygame.mixer.music.load(file_path + filename)
+
 pygame.mixer.music.play()
 
 while pygame.mixer.music.get_busy() == True:
     continue
+
