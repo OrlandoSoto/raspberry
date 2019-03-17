ab98b52facbca836b82c851a754e81a08b23aee1
Orlando soto-Pastor <Orlando.Soto-pastor@cfpb.gov>
2019-03-17 10:56:58 -0400

    Remove taking snapshot

--- my-motion.py
+++ my-motion.py
@@ -2,6 +2,7 @@
 import time
 import utils
 from gpiozero import MotionSensor
+import datetime
 
 snapshots_dir = 'snapshots'
 
@@ -11,10 +12,12 @@ def main():
     while (True):
         if(pir.wait_for_motion()):
             print("Motion detected")
-            snapshot_file = utils.take_snapshot(snapshots_dir)
+            #snapshot_file = utils.take_snapshot(snapshots_dir)
+            my_date = datetime.datetime.now().strftime("%B %d %Y %-H %M %S")
+            print(my_date)
             utils.play_sound()
-            utils.copy_snapshot(snapshot_file)
-            time.sleep(5)
+            #utils.copy_snapshot(snapshot_file)
+            time.sleep(10)
 
 
 if __name__ == '__main__':
