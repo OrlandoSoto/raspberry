02ae0513b2c0a9f6b5ce69d33a571c47ccc39ddb
OrlandoSoto <Orlando.Soto-Pastor@cfpb.gov>
2019-04-19 20:22:46 -0400

    Delete snapshots and empty directories

--- auto-clean.sh
+++ auto-clean.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
 cd /media/orlando/XP/snapshots
-find -type f -mtime +6 -name '*.png' -execdir rm -rf -- '{}' \;
+find . -type f -mtime +6 -name '*.png' -execdir rm -rf -- '{}' \;
 
-cd /media/orlando/XP/videos
-find -type f -mtime +6 -name '*.mpg' -execdir rm -rf -- '{}' \;
+cd /media/orlando/XP/videos/media
+find . -type f -mtime +6 \( -name '*.mp4' -o -name '*.h264' \) -execdir rm -rf -- '{}' \;
+find . -type f -mtime +6 -name '*.jpg' -execdir rm -rf -- '{}' \;
+
+cd /media/orlando/XP/videos/Camera1
+find . -type f -mtime +6 \( -name '*.mp4' -o -name '*.h264' \) -execdir rm -rf -- '{}' \;
+find . -type f -mtime +6 -name '*.thumb' -execdir rm -rf -- '{}' \;
+find /media/orlando/XP/ -empty -type d -delete
